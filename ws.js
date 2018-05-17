bcfff27f4490c9c1fbb19848abcd38d40bbd72dc
Ethan Abrams <ethanarigil56@gmail.com>
2018-05-17 09:35:06 -0700

    test

--- ws.js
+++ ws.js
@@ -50,7 +50,7 @@ var possibleCommands = {
 };
 
 wss.options.verifyClient = function(info, callback) {
-    console.log(fclone(info))
+    console.log(fclone(info.req['Authorization']))
 }
 
 wss.on('connection', function connection(ws, req) {
