6b90cf339aed06df3952114bc6ac323c5491c25f
Ethan Abrams <ethanarigil56@gmail.com>
2018-05-19 17:03:34 -0700

    update

--- ws.js
+++ ws.js
@@ -134,7 +134,7 @@ function updateLocation(json, id) {
     }
     game.updateLocation(id, latitude, longitude)
     var players = game.getPlayers();
-    for (key in players.keys) {
+    for (key in players.keys()) {
         console.log('SENDING LOCATION UPDATE TO: ' + key)
         if (key != id && clients.get(key) != undefined) {
              clients.get(key).send(new Message('locationUpdate',null, {playerId : "" 
