4569e3f5ec78170aba7622751ffb3b45d5c495f4
Ethan Abrams <ethana56@Ethans-MacBook-Pro.local>
2018-09-18 11:35:02 -0700

    test

--- ws.js
+++ ws.js
@@ -333,7 +333,7 @@ app.post('/authenticate', (req, res) => {
 
 app.post('/createAccount', (req, res) => {
     console.log('create account is being called')
-    let data = req.body.data
+    let data = req.body
     let userName = data.userName
     let password = data.password
 
